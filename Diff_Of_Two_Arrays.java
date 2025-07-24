6aa67cb77f789b3c3c6b22638b20e09f203aff73
Abhishek Pulate <abhishekpulate006@gmail.com>
2025-07-25 01:55:48 +0530

    Create Diff_Of_Two_Arrays.java

---
+++ Diff_Of_Two_Arrays.java
@@ -0,0 +1,108 @@
+package Arrays;
+import java.util.*;
+public class Diff_Of_Two_Arrays {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first number : ");
+		int num1 = sc.nextInt();
+		int cnt1 = count(num1);
+		int[] arr1 = new int[cnt1];
+		arr1 = add(num1,cnt1);
+		System.out.println(Arrays.toString(arr1));
+		
+
+		System.out.print("Enter Second number : ");
+		int num2 = sc.nextInt();
+		int cnt2 = count(num2);
+		int[] arr2 = new int[cnt2];
+		arr2 = add(num2,cnt2);
+		System.out.println(Arrays.toString(arr2));
+		
+		int[] result  = arraySubtraction(arr1,arr2);
+		
+//		int cnt = 0;
+//		for(int i = 0;i<res.length;i++)
+//		{
+//			if(res[i]>0)
+//			{
+//				break;
+//			}
+//			else
+//			{
+//				cnt++;
+//			}
+//		}
+//		int[] result =  new int[res.length-cnt];
+//		for(int i = cnt-1,j=0;i<result.length;i++,j++)
+//		{
+//			result[j] = res[i]; 
+//		}
+		
+		System.out.println("After successuful subtraction the result is as follows.");
+		System.out.println(Arrays.toString(result));
+		
+	}
+	public static int count(int num)
+	{
+		int cnt = 0;
+		for(int i = num;i>0;i/=10)
+		{
+			cnt++;
+		}
+		return cnt;
+	}
+	public static int[] add(int num,int cnt1)
+	{
+		int [] demo = new int[cnt1];
+		for(int i = num;i>0;i/=10)
+		{
+			int rem = i%10;
+			demo[cnt1-1] = rem;
+			cnt1--;
+		}
+		return demo;
+	}
+	public static int[] arraySubtraction(int[]a1,int[]a2)
+	{
+		int index1 = a1.length-1;
+		boolean flg = false;
+		int max = a2.length;
+		int[] diff = new int[max];
+		for(int i = max-1;i>=0;i--)
+		{
+			boolean carry = false;
+			if(a2[i]==(-1))
+			{
+				a2[i] +=10;
+				a2[i-1] -=1;
+			}
+			if(index1<0)
+			{
+				diff[i] = a2[i] ;
+			}
+			else if(a1[index1]>a2[i])
+			{
+			
+					
+					a2[i-1] -=1;
+					a2[i] +=10; 
+				
+				
+				diff[i] = a2[i] - a1[index1];
+				index1--;
+			}
+			else 
+			{
+				diff[i] = a2[i] - a1[index1];
+				
+				index1--;
+			}
+			
+		}
+		return diff;
+	}
+
+}
